fb24c471e9ae26b2852c48d0f9a0d75a37aa8f71
Shawn Gallea <shawngallea@gmail.com>
2020-10-17 03:14:41 -0400

    Remove debugging prints.

--- ConsoleApplication1/Opponent.cpp
+++ ConsoleApplication1/Opponent.cpp
@@ -29,16 +29,12 @@ void Opponent::RandomPieceRandomSpot(std::pair<int, int>& starting_spot, std::pa
 		if (blacks.size() == 0)
 			return;
 		int random_piece_index = rand() % blacks.size();
-		std::cout << "random piece index: " << random_piece_index << std::endl;
 		auto random_piece = blacks.at(random_piece_index);
 		auto movement_spots = random_piece->GetPossibleMovementSpots(board_, false);
-		std::cout << "Movement spots size: " << movement_spots.size() << std::endl;
 		
 		if (movement_spots.size() == 0) {
-			std::cout << "IF" << std::endl;
 			blacks.erase(blacks.begin() + random_piece_index);
 		} else {
-			std::cout << "ELSE" << std::endl;
 			auto random_spot = movement_spots.at(rand() % movement_spots.size());
 			board_->Move(random_piece->GetX(), random_piece->GetY(), random_spot.first, random_spot.second);
 			break;
